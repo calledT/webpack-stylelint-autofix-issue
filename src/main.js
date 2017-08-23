b4ff594f2079c417ff3eac207dfd79dd340a76e4
calledT <huanggengtao@gmail.com>
2017-08-23 23:56:49 +0800

    inital commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css';
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
